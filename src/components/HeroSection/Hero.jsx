0af505500026755cffcf792e7476fb9edd7a050e
Kowshik-hireyoo <kowshik.m@hireyoo.com>
2024-11-13 04:59:58 -0800

    second commit

--- src/components/HeroSection/Hero.jsx
+++ src/components/HeroSection/Hero.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import './Hero.css';
 import { CiSearch } from 'react-icons/ci'; // Importing the search icon
-
+import { SlOptionsVertical } from 'react-icons/sl'; // Importing the SlOptionsVertical icon
+import { GrCloudlinux } from "react-icons/gr";
+import { LiaDnaSolid } from "react-icons/lia";
+import { BsDatabaseCheck } from "react-icons/bs";
+import { BsGraphUpArrow } from "react-icons/bs";
 
 const HeroSection = () => {
   return (
@@ -19,8 +23,45 @@ const HeroSection = () => {
           
         <div className="hero-line"></div> {/* Line inside the rectangle */}
             </div> 
-             {/* New Rectangle 132 */}
-        <div className="hero-rectangle-132"></div>
+             {/* New Container-1 */}
+        <div className="hero-Container-1">
+        <h2 className="hero-container-title">Best Learning Program Across The World</h2>
+
+
+        <div className='display-tech-1'>
+          {/* Add the SlOptionsVertical icon here */}
+          <SlOptionsVertical className="ellipsis-icon" />
+          <GrCloudlinux className='web-tech'/>
+          <h3 className='web-title'>Web Development</h3>
+          <p className='web-p'>71.8M Learners</p>
+        </div>
+
+        <div className='display-tech-2'>
+          {/* Add the icon here */}
+          <SlOptionsVertical className="ellipsis-icon" />
+          <LiaDnaSolid  className='machin-lear'/>
+          <h3 className='machin-title'>Machine Learning</h3>
+          <p className='machin-p'>55.7M Learners</p>
+        </div>
+
+        <div className='display-tech-3'>
+          {/* Add the icon here */}
+          <SlOptionsVertical className="ellipsis-icon" />
+          <BsDatabaseCheck   className='data-sci'/>
+          <h3 className='data-title'>Data Science</h3>
+          <p className='data-p'>31.8M Learners</p>
+        </div>
+
+        <div className='display-tech-4'>
+          {/* Add the icon here */}
+          <SlOptionsVertical className="ellipsis-icon" />
+          <BsGraphUpArrow    className='business-an'/>
+          <h3 className='business-title'>Business Analytics</h3>
+          <p className='business-p'>41.4M Learners</p>
+        </div>
+
+        </div>
+        
       </div>
     </section>
   );
